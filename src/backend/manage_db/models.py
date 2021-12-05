cf488ae750b532817ba0a982cc0db9d5dd51a286
Jacob Feitelberg <j.e.feitelberg@gmail.com>
2021-12-05 17:08:02 -0500

    implemented getting the last time

--- src/backend/manage_db/models.py
+++ src/backend/manage_db/models.py
@@ -1,13 +1,15 @@
+from datetime import date
 from django.db import models
+from django.utils import timezone
+import os
 
 
 class Data(models.Model):
     ticker = models.CharField(unique=True,primary_key=True,max_length=20)
     last_updated = models.DateTimeField()
-    file_path = models.CharField(max_length=256)
-    last_date = models.DateTimeField()
-    download_link = models.CharField(max_length=256)
-    update_link = models.CharField(max_length=256)
+    file_path = models.CharField(max_length=256, default='')
+    download_link = models.CharField(max_length=256, default='')
+    update_link = models.CharField(max_length=256, default='')
     
     
     def update(self):
@@ -15,6 +17,35 @@ class Data(models.Model):
         Update the file with remote copy.
         """
         pass
+    
+    
+    def get_remote_time(self):
+        """
+        Gets the last datetime of the remote data.
+        """
+        # Download remote updated data
+        # 
+        pass
+    
+    
+    def __get_time(self, file_path):
+        with open(file_path, "rb") as file:
+            # Go to the end of the file before the last break-line
+            file.seek(-2, os.SEEK_END) 
+            # Keep reading backward until you find the next break-line
+            while file.read(1) != b'\n':
+                file.seek(-2, os.SEEK_CUR) 
+            last_line = file.readline().decode()
+            return timezone.datetime.strptime(last_line[:last_line.find(',')], "%Y-%m-%d %H:%M:%S")
+    
+    
+    def get_local_time(self):
+        """
+        Gets the last datetime of the local data.
+        """
+
+        return self.__get_time(self.file_path)
+        
     
     
     def download(self):
