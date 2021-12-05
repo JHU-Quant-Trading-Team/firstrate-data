e4a8f193b0fac5d53705dd6ab533f88b9ffdf144
Jacob Feitelberg <j.e.feitelberg@gmail.com>
2021-12-05 12:07:28 -0500

    added skeleton code for updating and downloading files

--- src/backend/manage_db/models.py
+++ src/backend/manage_db/models.py
@@ -5,4 +5,21 @@ class Data(models.Model):
     ticker = models.CharField(unique=True,primary_key=True,max_length=20)
     last_updated = models.DateTimeField()
     file_path = models.CharField(max_length=256)
+    last_date = models.DateTimeField()
+    url_link = models.CharField(max_length=256)
+    
+    
+    def update(self):
+        """
+        Update the file with remote copy.
+        """
+        pass
+    
+    
+    def download(self):
+        """
+        Download the remote copy.
+        """
+        pass
+        
     
