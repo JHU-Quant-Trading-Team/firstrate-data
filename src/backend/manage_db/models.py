2c1e77f06ea403eb11a0535133ff267128650ee9
Jacob Feitelberg <j.e.feitelberg@gmail.com>
2021-12-07 16:54:39 -0500

    added update code

--- src/backend/manage_db/models.py
+++ src/backend/manage_db/models.py
@@ -24,12 +24,12 @@ class DataBundle(models.Model):
             os.makedirs(self.root_path)
         
         # Download remote data
-        zip_file_name = f'{self.root_path}temp.zip'
+        zip_file_name = self.root_path + 'temp.zip'
         urllib.request.urlretrieve(self.update_link, zip_file_name)
         
         # Unzip remote data
         with zipfile.ZipFile(zip_file_name, 'r') as zip_ref:
-            zip_ref.extractall(f'{self.root_path}')
+            zip_ref.extractall(self.root_path)
             
         # Remove zip file
         os.remove(zip_file_name)
@@ -37,9 +37,28 @@ class DataBundle(models.Model):
     
     def update(self):
         """
-        Update the file with remote copy.
+        Update the files with remote copy.
         """
-        pass
+        # Create temporary folder to extract remote data to
+        temp_root = self.root_path + 'temp/'
+        if not os.path.exists(temp_root):
+            os.makedirs(temp_root)
+            
+        # Download remote updated data
+        urllib.request.urlretrieve(self.update_link, temp_root + '/temp.zip')
+        
+        # Unzip temp data
+        with zipfile.ZipFile(temp_root + '/temp.zip', 'r') as zip_ref:
+            zip_ref.extractall(temp_root)
+        
+        # Update each object
+        ## Get all data objects related to this bundle
+        data_objects = Data.objects.filter(bundle=self)
+        for data in data_objects:
+            data.update(temp_root + data.file_name)
+        
+        # Delete temporary folder
+        shutil.rmtree(temp_root)
     
     
     def get_remote_time(self):
@@ -48,28 +67,29 @@ class DataBundle(models.Model):
         """
         
         # Create temporary folder to extract remote data to
-        if not os.path.exists('temp/'):
-            os.makedirs('temp/')
+        temp_root = self.root_path + 'temp/'
+        if not os.path.exists(temp_root):
+            os.makedirs(temp_root)
         
         # Download remote updated data
-        urllib.request.urlretrieve(self.update_link, 'temp/temp.zip')
+        urllib.request.urlretrieve(self.update_link, temp_root + 'temp.zip')
         
         # Unzip temp data
-        with zipfile.ZipFile('temp/temp.zip', 'r') as zip_ref:
-            zip_ref.extractall('temp/')
+        with zipfile.ZipFile(temp_root + 'temp.zip', 'r') as zip_ref:
+            zip_ref.extractall(temp_root)
         
         # Get the remote time
         ## Get all data objects related to this bundle
         data_objects = Data.objects.filter(bundle=self)
         
         ## Get times from data files
-        times = [Data.__get_time(f'temp/{data.file_name}') for data in data_objects]
+        times = [Data.__get_time(temp_root + data.file_name) for data in data_objects]
         
         ## Get the most recent time
         remote_time = max(times)
         
         # Delete temporary folder
-        shutil.rmtree('temp/')
+        shutil.rmtree(temp_root)
         
         return remote_time
     
@@ -86,21 +106,31 @@ class DataBundle(models.Model):
 
         # Return the most recent time
         return max(times)
-    
+
+
 class Data(models.Model):
     ticker = models.CharField(unique=True,primary_key=True,max_length=20)
     file_name = models.CharField(max_length=256, default="")
     bundle = models.ForeignKey(DataBundle, on_delete=models.CASCADE)
     
     
+    def update(self, remote_file: str):
+        """
+        Update the file with remote copy.
+        """
+        # Open the remote file
+        with open(remote_file, "r") as f_remote, open(f'{self.bundle.root_path}{self.file_name}') as f_local:
+            f_local.writelines(f_remote.readlines())
+    
+    
     @staticmethod
-    def __get_time(file_path):
-        with open(file_path, "rb") as file:
+    def __get_time(file_path: str):
+        with open(file_path, "rb") as f:
             # Go to the end of the file before the last break-line
-            file.seek(-2, os.SEEK_END) 
+            f.seek(-2, os.SEEK_END) 
             # Keep reading backward until you find the next break-line
-            while file.read(1) != b'\n':
-                file.seek(-2, os.SEEK_CUR) 
-            last_line = file.readline().decode()
+            while f.read(1) != b'\n':
+                f.seek(-2, os.SEEK_CUR) 
+            last_line = f.readline().decode()
             return timezone.datetime.strptime(last_line[:last_line.find(',')], "%Y-%m-%d %H:%M:%S")
     
